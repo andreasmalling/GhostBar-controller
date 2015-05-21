07e1956a2203cbfc4734029e90d309a6dc175c52
Andreas Malling ØStergaard <amao@amao.dk>
2015-05-21 13:49:13 +0200

    Add Beacon transmission toggle

--- app/src/main/java/f2015/itsmap/ghostbar_controller/MainActivity.java
+++ app/src/main/java/f2015/itsmap/ghostbar_controller/MainActivity.java
@@ -4,14 +4,45 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends Activity {
+
+    private Beacon beacon;
+    private BeaconTransmitter beaconTransmitter;
+
+    private boolean transmitting = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int beaconSupport = BeaconTransmitter.checkTransmissionSupported(getApplicationContext());
+
+        if(beaconSupport == BeaconTransmitter.SUPPORTED) {
+            beacon = new Beacon.Builder()
+                    .setId1(getResources().getString(R.string.beacon_id1))
+                    .setId2(getResources().getString(R.string.beacon_id2))
+                    .setId3(getResources().getString(R.string.beacon_id3))
+                    .setManufacturer(0x0118) // Radius Networks.  Change this for other beacon layouts
+                    .setTxPower(-59)
+                    .setDataFields(Arrays.asList(new Long[]{0l})) // Remove this for beacon layouts without d: fields
+                    .build();
+
+            BeaconParser beaconParser = new BeaconParser()
+                    .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+
+            beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
+        }
+
     }
 
 
@@ -31,9 +62,23 @@ public class MainActivity extends Activity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            toggleTransmit();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void toggleTransmit() {
+        if(transmitting) {
+            beaconTransmitter.stopAdvertising();
+            transmitting = false;
+            Toast.makeText(getApplicationContext(), "Beacon transmission stopped", Toast.LENGTH_SHORT).show();
+        } else {
+            // TODO: check if bluetooth is enabled
+            beaconTransmitter.startAdvertising(beacon);
+            transmitting = true;
+            Toast.makeText(getApplicationContext(), "Beacon transmission started", Toast.LENGTH_SHORT).show();
+        }
     }
 }
